de8e41eb3428373fa9b54abfd11e34f069812ef0
Kira Hartlage <kira.hartlage@gmail.com>
2020-11-23 14:55:16 -0500

    improved stylign

--- components/ColorButton.js
+++ components/ColorButton.js
@@ -8,7 +8,7 @@ export default function ColorButton({ backgroundColor, onPress = f => f }) {
             underlayColor="orange"
         >
             <View style={styles.row}>
-                <View style={[styles.sample, {backgroundColor: "yellow" }]}/>
+                <View style={[styles.sample, { backgroundColor }]} />
                 <Text style={styles.buttonText}>{backgroundColor}</Text>
             </View>
         </TouchableHighlight>
@@ -17,25 +17,26 @@ export default function ColorButton({ backgroundColor, onPress = f => f }) {
 
 const styles = StyleSheet.create({
     button: {
-      margin: 10,
-      padding: 10,
-      borderWidth: 2,
-      borderRadius: 10,
-      alignSelf: "stretch",
-      backgroundColor: "rgba(255, 255, 255, 0.8)"
+        margin: 10,
+        padding: 10,
+        borderWidth: 2,
+        borderRadius: 10,
+        alignSelf: "stretch",
+        backgroundColor: "rgba(255, 255, 255, 0.8)"
     },
     buttonText: {
-      fontSize: 30,
-      textAlign: "center"
+        fontSize: 30,
+        textAlign: "center",
+        marginLeft: 15
     },
     row: {
         flexDirection: "row",
         alignItems: "center"
     },
     sample: {
-      height: 20,
-      width: 20,
-      borderRadius: 10,
-      backgroundColor: "white"
+        height: 20,
+        width: 20,
+        borderRadius: 10,
+        backgroundColor: "white"
     }
-  });
+});
