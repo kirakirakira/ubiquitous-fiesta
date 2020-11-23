64d3bcb0a1375424f92a88a01b9665993b67b188
Kira Hartlage <kira.hartlage@gmail.com>
2020-11-23 14:59:21 -0500

    used a flatlist

--- App.js
+++ App.js
@@ -1,29 +1,26 @@
 import React, { useState } from "react";
-import { Text, View, Button, StyleSheet } from "react-native";
+import { Text, View, Button, StyleSheet, FlatList } from "react-native";
 import ColorButton from "./components/ColorButton";
+import defaultColors from "./data/defaultColors.json"
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("white");
   const [count, setCount] = useState(0);
 
   return (
-    <View style={[styles.container, { backgroundColor }]}>
-      <ColorButton backgroundColor="red" onPress={setBackgroundColor}/>
-      <ColorButton backgroundColor="blue" onPress={setBackgroundColor}/>
-      <ColorButton backgroundColor="green" onPress={setBackgroundColor}/>
-      <ColorButton backgroundColor="yellow" onPress={setBackgroundColor}/>
-      <ColorButton backgroundColor="purple" onPress={setBackgroundColor}/>
-      <Text>You clicked {count} times.</Text>
-      <Button title="Click me" onPress={() => setCount(count + 1)}></Button>
-    </View>
+    <FlatList style={[styles.container, { backgroundColor }]}
+      data={defaultColors}
+      renderItem={({item}) => {
+        return (
+          <ColorButton key={item.id} backgroundColor={item.color} onPress={setBackgroundColor}/>
+        )
+      }} />     
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
+    display: "flex"
   }
 });
